d6d97275f6608ddda6ce2e05c221286a9191104c
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2022-06-28 04:15:55 -0500

    Work page data migrated to database

---
+++ functions/load_work.php
@@ -0,0 +1,48 @@
+<?php
+    include('database.php');
+    $dblink=dbconnect('web_tech');
+    $sql = "select * from `work_entries`";
+    $result = $dblink->query($sql) or die("Something went wrong with $sql");
+
+    echo '<br>';
+    if($result->num_rows == 0){
+        echo '<div class=\"row justify-content-center\">
+                <p class=\"help-block comment-help col-11\">Nothing to see here</p>
+            </div>';
+    }else{
+        while($entry = mysqli_fetch_assoc($result)){
+            $entries[] = $entry;
+        }
+
+        foreach($entries as $key => $entry){
+            $id = $entry['id'];
+            $logo = $entry['logo'];
+            $logoSize = 60;
+            $name = $entry['name'];
+            $location = $entry['location'];
+            $startDate = date("M Y", strtotime($entry['start_dt']));
+            $endDate = date("M Y", strtotime($entry['end_dt']));
+            $title = $entry['title'];
+            $duties = $entry['duties'];
+
+            echo "<div class=\"row\">
+                    <div class=\"col\" align=\"center\">
+                        <img src=\"$logo\" alt=\"$name Logo\" height=\"$logoSize\" />
+                        <h3>$name</h3>
+                        <h4>$startDate - $endDate | $location</h4>
+                    </div>
+                    <div class=\"col\">
+                        <dl>
+                            <dt>Title:</dt>
+                            <dd>$title</dd>
+                            <dt>Duties:</dt>
+                            <dd>$duties</dd>
+                        </dl>
+                    </div>
+                </div>";
+
+            if($key !== array_key_last($entries))
+                echo "<hr>";
+        }
+    }
+?>
--- pages/work.php
+++ pages/work.php
@@ -1,108 +1,17 @@
-<?php
-    echo '<div class="container">
-        <br>
-        <div class="row">
-            <div class="col" align="center">
-                <br>
-                <!-- img src="./assets/images/ups_logo.png" alt="UPS" height="60" -->
-                <img src="https://logos-download.com/wp-content/uploads/2016/08/UPS_logo_logotype.png" alt="UPS" height="60">
-                <h3>UPS Warehouse</h3>
-                <h4>Jul 2020 - Jan 2021 | San Antonio, TX</h4>
-            </div>
-            <div class="col">
-                <dl>
-                    <dt>Title:</dt>
-                    <dd>Data Acquisition</dd>
-                    <dt>Duties:</dt>
-                    <dd>Corrected addresses on packages as well as completed various other tasks in the small sort area.
-            Would often be moved to other areas of the warehouse to complete duties outside of my
-            position, such as debagging, loading and unloading trucks, and transported irregular 
-            packages.</dd>
-                </dl>
-            </div>
-        </div>
-
-        <hr>
-        
-        <div class="row">
-            <div class="col" align="center">
-                <br>
-                <!-- img src="./assets/images/seaworld_logo.png" alt="SeaWorld" height="60" -->
-                <img src="https://i1.wp.com/www.concretepavements.org/wp-content/uploads/2016/02/SeaWorld_San_Antonio_logo.svg_.png?ssl=1" alt="SeaWorld" height="60">
-                <h3>SeaWorld San Antonio</h3>
-                <h4>Jun 2019 - Aug 2019 | San Antonio, TX</h4>
-            </div>
-            <div class="col">
-                <dl>
-                    <dt>Title:</dt>
-                    <dd>Park Prep (night)</dd>
-                    <dt>Duties:</dt>
-                    <dd>Cleaned Bathrooms, Picked up litter, Compacted Trash and Recyclables, and completed various 
-            other janitorial duties</dd>
-                </dl>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col" align="center">
-                <br>
-                <img src="./assets/images/maxfoodmart_logo_trans.png" alt="MaxFood" height="60">
-                <h3>Max Food Mart</h3>
-                <h4>Dec 2017 - Mar 2018 | San Antonio, TX</h4>
-            </div>
-            <div class="col">
-                <dl>
-                    <dt>Title:</dt>
-                    <dd>Cashier (management)</dd>
-                    <dt>Duties:</dt>
-                    <dd>Tilled counter, managed storefront, restocked, swept floor, took out trash, looked after store.
-            I worked alone so this was essentially a management position as I would be the only employee
-            in the store a majority of the time.</dd>
-                </dl>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col" align="center">
-                <br>
-                <!-- img src="./assets/images/billmillersbbq_logo.png" alt="BMBBQ" height="60" -->
-                <img src="https://2rq17orsr961kf85lee3hka9-wpengine.netdna-ssl.com/wp-content/themes/bill-miller-theme/img/main-logo.png" alt="BMBBQ" height="60">
-                <h3>Bill Miller\'s BBQ</h3>
-                <h4>Mar 2014 - Aug 2014 | San Antonio, TX</h4>
-            </div>
-            <div class="col">
-                <dl>
-                    <dt>Title:</dt>
-                    <dd>Bus and Kitchen</dd>
-                    <dt>Duties:</dt>
-                    <dd>Bused tables, fried chicken, washed dishes, swept floors and parking garage, 
-            cleaned bathrooms, took out trash</dd>
-                </dl>
-            </div>
-        </div>
-
-        <hr>
-
-        <div class="row">
-            <div class="col" align="center">
-                <br>
-                <!-- img src="./assets/images/bolnersfiesta_logo.png" alt="Fiesta" height="60" -->
-                <img src="https://www.fiestaspices.com/wp-content/uploads/2015/03/logo.png" alt="Fiesta" height="60">
-                <h3>Bolner\'s Fiesta Products</h3>
-                <h4>Feb 2014 - Mar 2014 | San Antonio, TX</h4>
-            </div>
-            <div class="col">
-                <dl>
-                    <dt>Title:</dt>
-                    <dd>Packaging</dd>
-                    <dt>Duties:</dt>
-                    <dd>Packaged various spices and peppers</dd>
-                </dl>
-            </div>
-        </div>
-    </div>';
-?>
+<html>
+    <body>
+    <script>
+        $.ajax({
+            type:'post',
+            url:'../functions/load_work.php',
+            success:function(data){
+                $('#subContent').html(data);
+            }
+        })
+    </script>
+    <br>
+    <div id="container">
+        <div id="subContent"></div>
+    </div>
+    </body>
+</html>
