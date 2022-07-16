841ce008dd57b06c85f8de32f17c51513a562b98
SomeGuyNamedJon <jonathanvillarreal1995@outlook.com>
2022-07-16 04:46:49 -0500

    Configured home to be database drivern

---
+++ functions/load_home.php
@@ -0,0 +1,74 @@
+<?php
+    include('database.php');
+    $dblink=dbconnect('web_tech');
+
+    $sql = "select * from `home_img`";
+    $result = $dblink->query($sql) or die("Something went wrong with $sql");
+
+    if($result->num_rows != 0){
+        $i = 0;
+        echo '<div class="row">';
+        while($img = mysqli_fetch_assoc($result)){
+            $title = $img['title'];
+            $image = $img['image'];
+            $row = $img['image'];
+            $imgId = strtolower(str_replace(' ', '', $title));
+
+            if($row > $i){
+                echo '</div><div class="row">';
+                $i = $row;
+            }
+
+            echo '<div class="col">';
+            echo "<h2>$title</h2>";
+            echo '<img id="'.$imgId.'" src="'.$image.'" alt="'.$title.'" height="350"></div>';
+        }
+        echo '</div>';
+    }
+
+    $sql = "select * from `home_about`";
+    $result = $dblink->query($sql) or die("Something went wrong with $sql");
+
+    if($result->num_rows == 0){
+        echo '<div class=\"row justify-content-center\">
+                <p class=\"help-block comment-help col-11\">Nothing to see here</p>
+            </div>';
+    }else{
+        while($entry = mysqli_fetch_assoc($result)){
+            $id = $entry['id'];
+            $title = $entry['title'];
+            $bulk = $entry['bulk'];
+            $image = $entry['image'];
+            $type = $entry['list_type'];
+            $imgId = strtolower(str_replace(' ', '', $title));
+
+            echo "<h2>$title</h2>";
+
+            if($image)
+                echo '<img id="'.$imgId.'" src="'.$image.'" alt="'.$title.'" height="100"></div>';
+
+            if($type == null)
+                echo "<ul><p>$bulk</b></ul>";
+            else{
+
+                echo "<$type><p>$bulk</p>";
+
+                echo '<div class="row">';
+                $sql = "select * from `about_bulletpoints` where `title_id`=$id";
+                $subresult = $dblink->query($sql) or die("Something went wrong with $sql");
+                while($bulletpoint = mysqli_fetch_assoc($subresult)){
+                    $desc = $bulletpoint['description'];
+
+                    if($type == 'dl'){
+                        $term = $bulletpoint['term'];
+                        echo "<dt>$term</dt>";
+                        echo "<dd>$desc</dd>";
+                    }else{
+                        echo "<li>$desc</li>";
+                    }
+                }
+                echo "</$type></div>";
+            }
+        }
+    }
+?>
--- pages/home.php
+++ pages/home.php
@@ -1,41 +1,17 @@
 <html>
-    <div class="row">
-        <div class="col">
-            <h2>My Face</h2>
-            <img id="myFace" src="./assets/images/Me.jpg" alt="myface" height="350">
-        </div>
-        <div class="col">
-            <h2>My System</h2>
-            <img id="neofetch" src="./assets/images/neofetch.png" height="350">
-            <!-- <script src="https://asciinema.org/a/473578.js" id="asciicast-473578" async></script> -->
-        </div>
+    <body>
+    <script>
+        $.ajax({
+            type:'post',
+            url:'../functions/load_home.php',
+            success:function(data){
+                $('#subContent').html(data);
+            }
+        })
+    </script>
+    <br>
+    <div id="container">
+        <div id="subContent"></div>
     </div>
-    
-    <div class="row">
-        <div class="col">
-            <dl>
-            <h2>About Me:</h2>
-            <dd>I am a recent graduate of the University of Texas at San Antonio. I attained my Bachelor\'s 
-                of Computer Science with a GPA of 3.31.
-                I am a linux gamer and am passionate about FOSS and Open Source in general.
-                Looking forward to graduating and entering the work force to expand my knowledge and put 
-                what I have learned in University into practice.
-            </dd>
-            </dl>
-            <h2>Personal Experience:</h2>
-            <ul>
-            <li>Troubleshooted various systems from Windows to Linux</li>
-            <li>Built systems for personal use</li>
-            <li>Have programed in C/C++, C#, Java, PHP, Javascript, nodeJS, Lisp, Python, 
-                x86, bash, awk, SQL, HTML, and CSS
-            </li>
-            </ul>
-            <h2>Additional Skills:</h2>
-            <ul>
-            <li>Bilingual in English and Spanish</li>
-            <li>Fast learner</li>
-            <li>Excellent communication skills</li>
-            </ul>
-        </div>
-    </div>
+    </body>
 </html>
